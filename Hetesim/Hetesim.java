c4403aa35d8aaad06d7df2690e1017303ccf1015
Mkaruso <guillem.castro@hotmail.com>
2016-04-16 17:45:28 +0200

    Añadido Hetesim.java
    Añadida clase + constructor + getter del grafo

---
+++ Hetesim/Hetesim.java
@@ -0,0 +1,17 @@
+package Hetesim;
+import Graf.Graf;
+
+public class Hetesim {
+	
+	private Graf graf;
+	
+	public Hetesim(Graf graf) {
+		this.graf = graf;
+	}
+	
+	public Graf getGraf() {
+		return this.graf;
+	}
+	
+	
+}
