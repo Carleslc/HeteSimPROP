02ee1964e6ce9ccfeb1f69b29da1e6a9042c5d87
Mkaruso <guillem.castro@hotmail.com>
2016-04-17 14:36:28 +0200

    Añadida función matriusPath()
    - matriusPath() devuelve una lista con las matrices necesarias para
    calcular hetesim (ya normalizadas)
    - añadidos algunos comentarios

--- Hetesim/Hetesim.java
+++ Hetesim/Hetesim.java
@@ -1,5 +1,9 @@
 package Hetesim;
 
+/**
+ * @authors Guillem Castro, Carlos Lazaro
+ */
+
 import Graf.Graf;
 import Matriu.Matriu;
 
@@ -33,7 +37,12 @@ public class Hetesim {
 		if (path == null) {
 			throw new IllegalArgumentException("'path' no pot ser null");
 		}
-		return (Matriu<Double>) clausures.get(path);
+		if (clausures.containsKey(path)) {
+			return (Matriu<Double>) clausures.get(path);
+		}
+		
+		//calcular clausura...
+		return null;
 	}
 	
 	/**
@@ -59,6 +68,7 @@ public class Hetesim {
 			return clausura.get(a.getId(), b.getId());
 		}
 		
+		//calcular HeteSim...
 		
 		return 0; //Esta linea hay que borrarla
 	}
@@ -72,9 +82,101 @@ public class Hetesim {
 			throw new IllegalArgumentException("El tipus del node no coincideix amb el primer del 'path'");
 		}
 		
-		if (clausures.containsKey(path)) {
-			ArrayList<Double> fila = clausures.get(path).getFila(n.getId());
+		//calcular HeteSim...
+		
+		return null; //Esta linea hay que borrarla
+	}
+	
+	/**
+	 * Devuelve una lista con todas las matrices necesarias para calcular
+	 * HeteSim del path dado
+	 * @param path Path del que se quiere obtener sus matrices
+	 * @return Devuelve un ArrayList<Matriu<Double>> con las matrices correspondientes
+	 * a 'path'
+	 */
+	private ArrayList<Matriu<Double>> matriusPath(String path) {
+		int longitud = path.length() - 1;
+		ArrayList<Matriu<Double>> mPath = null;
+		if (longitud%2 == 0) { //longitud par -> no hace falta añadir Empty
+			for (int i = 0; i < longitud; ++i) {
+				mPath = new ArrayList<>(longitud);
+				char charAti = path.charAt(i);
+				if (charAti == 'A') {
+					Matriu<Double> m = creaMatriuDouble(graf.consultarMatriuPaperAutor().transposada());
+					if (i+1 <= (longitud)/2) {
+						m.normalitzaPerFiles();
+						mPath.set(i, m);
+					}
+					else {
+						m.normalitzaPerColumnes();
+						mPath.set(i, m);
+					}
+				}
+				else if (charAti == 'C') {
+					Matriu<Double> m = creaMatriuDouble(graf.consultarMatriuPaperConferencia().transposada());
+					if (i+1 <= (longitud)/2) {
+						m.normalitzaPerFiles();
+						mPath.set(i, m);
+					}
+					else {
+						m.normalitzaPerColumnes();
+						mPath.set(i, m);
+					}
+				}
+				else if (charAti == 'T') {
+					Matriu<Double> m = creaMatriuDouble(graf.consultarMatriuPaperTerme().transposada());
+					if (i+1 <= (longitud)/2) {
+						m.normalitzaPerFiles();
+						mPath.set(i, m);
+					}
+					else {
+						m.normalitzaPerColumnes();
+						mPath.set(i, m);
+					}
+				}
+				else if (charAti == 'P') {
+					char charAti1 = path.charAt(i+1);
+					Matriu<Double> m = null;
+					if (charAti1 == 'A') {
+						m = creaMatriuDouble(graf.consultarMatriuPaperAutor());
+					}
+					if (charAti1 == 'C') {
+						m = creaMatriuDouble(graf.consultarMatriuPaperConferencia());
+					}
+					if (charAti1 == 'T') {
+						m = creaMatriuDouble(graf.consultarMatriuPaperTerme());
+					}
+					if (i+1 <= (longitud)/2) {
+						m.normalitzaPerFiles();
+						mPath.set(i, m);
+					}
+					else {
+						m.normalitzaPerColumnes();
+						mPath.set(i, m);
+					}
+				}
+			}
 		}
-		return null;
+		else {
+			mPath = new ArrayList<>(longitud+1);
+			//añadir elemento Empty y poner matrices en ArrayList
+		}
+		return mPath;
+	}
+	
+	/*
+	 * Esta funcion es necesaria hasta que tengamos una creadora
+	 * por copia o que la funcion normalizar devuelva una Matriu<Double>
+	 */
+	private Matriu<Double> creaMatriuDouble(Matriu<Byte> m) {
+		int files = m.getFiles();
+		int cols = m.getColumnes();
+		Matriu<Double> res = new Matriu<Double>(files, cols, 0d);
+		for (int i = 0; i < files; ++i) {
+			for (int j = 0; j < cols; ++j) {
+				res.set(i, j, (double) m.get(i, j));
+			}
+		}
+		return res;
 	}
 }
