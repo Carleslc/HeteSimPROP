21c7684d33e2f81e742f0cee171a6a98b5b678b1
cclaverol <cclaverol.7@gmail.com>
2016-05-22 11:57:37 +0200

    update GestorDades

--- presentacio/GestorDades.java
+++ presentacio/GestorDades.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowEvent;
@@ -13,47 +14,50 @@ public class GestorDades extends JFrame {
 
 	private static final long serialVersionUID = 2697285126957563652L;
 	private JPanel contentPane;
-
+	
 	public GestorDades(ControladorPresentacioDomini ctrl) {
+		config();
+		
+		JButton btnGestorConjDades = new JButton("Gestor de conjunts de dades");
+		btnGestorConjDades.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				GestorConjuntDades frame = new GestorConjuntDades();
+				configurarNovaFinestra(frame);
+			}
+		});
+		btnGestorConjDades.setBounds(102, 60, 223, 35);
+		contentPane.add(btnGestorConjDades);
+		
+		JButton btnGestorRelacions = new JButton("Gestor de relacions");
+		btnGestorRelacions.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				GestorRelacions frame = new GestorRelacions(ctrl);
+				configurarNovaFinestra(frame);
+			}
+		});
+		btnGestorRelacions.setBounds(102, 143, 223, 35);
+		contentPane.add(btnGestorRelacions);
+	}
+	
+	private void config() {
+		setTitle("Gestor de dades");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JButton btnNewButton = new JButton("Gestor de conjunts de dades");
-		btnNewButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				GestorConjuntDades frame = new GestorConjuntDades(ctrl);
-				frame.setVisible(true);
-				setVisible(false);
-				frame.addWindowListener(new WindowAdapter() {
-					public void windowClosed(WindowEvent e) {
-						setVisible(true);
-					}
-				});
+	}
+	
+	private void configurarNovaFinestra(JFrame frame) {
+		frame.setVisible(true);
+		setVisible(false);
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosed(WindowEvent e) {
+				setVisible(true);
 			}
 		});
-		btnNewButton.setBounds(102, 60, 223, 35);
-		contentPane.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Gestor de relacions");
-		btnNewButton_1.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				GestorRelacions frame = new GestorRelacions(ctrl);
-				frame.setVisible(true);
-				setVisible(false);
-				frame.addWindowListener(new WindowAdapter() {
-					public void windowClosed(WindowEvent e) {
-						setVisible(true);
-					}
-				});
-			}
-		});
-		btnNewButton_1.setBounds(102, 143, 223, 35);
-		contentPane.add(btnNewButton_1);
 	}
 }
