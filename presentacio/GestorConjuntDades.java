967e2bb9f8526ffa78ff7fa0d36fde0a87e30bb6
arnaubadia <arnaubadia96@gmail.com>
2016-05-23 09:54:08 +0200

    update

--- presentacio/GestorConjuntDades.java
+++ presentacio/GestorConjuntDades.java
@@ -11,6 +11,8 @@ import javax.swing.JButton;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 
 public class GestorConjuntDades extends JFrame {
@@ -25,7 +27,8 @@ public class GestorConjuntDades extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					GestorConjuntDades frame = new GestorConjuntDades();
+					ControladorPresentacioDomini ctrl = new ControladorPresentacioDomini();
+					GestorConjuntDades frame = new GestorConjuntDades(ctrl);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -37,14 +40,16 @@ public class GestorConjuntDades extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public GestorConjuntDades() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public GestorConjuntDades(ControladorPresentacioDomini ctrl) {
+		this.ctrl = ctrl;
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		//crear button
 		JButton btnNewButton = new JButton("Crear nou Conjunt de Dades");
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			@Override
@@ -61,18 +66,33 @@ public class GestorConjuntDades extends JFrame {
 		btnNewButton.setBounds(116, 48, 229, 60);
 		contentPane.add(btnNewButton);
 		
+		
+		//modificar button
 		JButton btnNewButton_1 = new JButton("Modificar Conjunt de Dades");
 		btnNewButton_1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				InformacioAddicional x = new InformacioAddicional();
+				//ModificarConjuntDades x = new ModificarConjuntDades(ctrl);
+				//x.setVisible(true);
 			}
 		});
 		btnNewButton_1.setBounds(116, 115, 229, 60);
 		contentPane.add(btnNewButton_1);
 		
+		
+		//importar
 		JButton btnNewButton_2 = new JButton("Importar Conjunt de Dades");
+		btnNewButton_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Importar x = new Importar();
+				x.setVisible(true);
+			}
+		});
 		btnNewButton_2.setBounds(116, 187, 229, 53);
 		contentPane.add(btnNewButton_2);
 	}
+	
+	
+	
 }
